14886de4b1ab5d8ec3eb8d1617517495edc704a9
Stew-000-1-0-011 <92617876+Stew-000-1-0-011@users.noreply.github.com>
2022-10-26 15:32:24 +0900

    portsにアクセスできるように。

--- include/topic_mamager.hpp
+++ include/topic_mamager.hpp
@@ -30,17 +30,30 @@ namespace arduino_bridge
 	{
 		struct TopicData
 		{
-			const u8 id;
-			const u8 size;
+			TopicId id;
+			u8 size;
 			std::vector<asio::serial_port *> ports;
 			boost::shared_mutex mutex{};
+
+			TopicData(const TopicData& obj) noexcept
+			{
+				boost::lock_guard lock{obj.mutex};
+				id = obj.id;
+				size = obj.size;
+				ports = obj.ports;
+			}
 		};
 
 		std::unordered_map<std::string, TopicData> name_to_data{};
-		std::map<u8, std::string>  id_to_name{};
+		std::map<TopicId, std::string>  id_to_name{};
 		boost::shared_mutex mutex{};
 
 	public:
+		TopicManager() = default;
+		TopicManager(const TopicManager&) = delete;
+		TopicManager& operator=(const TopicManager&) = delete;
+		TopicManager(TopicManager&) = delete;
+		TopicManager& operator=(TopicManager&&) = delete;
 
 		// 本来は例外を使うべきなのかもしれないが、tryされなそうなのでやめた。
 		// registerが予約語なので...
@@ -72,12 +85,6 @@ namespace arduino_bridge
 			return true;
 		}
 
-		TopicId get_untied_id() const noexcept
-		{
-			boost::shared_lock lock{mutex};
-			return get_untied_id_nonlock();
-		}
-
 		bool unregist(const boost::string_view topic_name, asio::serial_port *const port) noexcept
 		{
 			{
@@ -107,7 +114,26 @@ namespace arduino_bridge
 			return false;
 		}
 
+		std::string get_topic_name(const TopicId id)
+		{
+			boost::shared_lock lock{mutex};
+			return id_to_name[id];
+		}
+
+		TopicData get_topic_data(boost::string_view name)
+		{
+			boost::shared_lock lock{mutex};
+			return name_to_data[name];
+		}
+
+
 	private:
+		TopicId get_untied_id() const noexcept
+		{
+			boost::shared_lock lock{mutex};
+			return get_untied_id_nonlock();
+		}
+
 		TopicId get_untied_id_nonlock() const noexcept
 		{
 			if(id_to_name.empty())
@@ -119,7 +145,7 @@ namespace arduino_bridge
 
 			for(auto iter_next = id_to_name.cbegin() + 1; iter_next != id_to_name.cend(); ++iter_prev, ++iter_next)
 			{
-				if(iter_prev->first + 1 < iter_next->first && iter_prev->first + 1 != CRSLib::to_underlying(TopicId::null))
+				if(iter_prev->first + 1 < iter_next->first && CRSLib::to_underlying(iter_prev->first) + 1 != CRSLib::to_underlying(TopicId::null))
 				{
 					return static_cast<TopicId>(iter_prev->first + 1);
 				}
